efb6934e7d2ea9c7749f8a57801930e62de58f06
Alex Forrence <aforren1@jhu.edu>
2016-03-15 17:28:24 -0400

    Added helper for finding active devices

--- R/serial.R
+++ R/serial.R
@@ -1,8 +1,11 @@
 #' Writes data to serial interface. 
 #' 
 #' @param con serial connection
-#' @param dat data string to write on the serial interface. At the moment this 
-#'            must be a character string \code{"..."}. See the example section in \code{\link{serial}}.
+#' @param dat data string to write on the serial interface. This can either
+#'                be a character string or any R object that can be coerced
+#'                to a character string by the \code{\link{toString}} function,
+#'                though the latter occurs with a warning. 
+#'                See the example section in \code{\link{serial}}.
 #' 
 #' @usage write.serialConnection(con, dat)
 #' @seealso \code{\link{serial}}
@@ -12,9 +15,13 @@
 #'  \dontrun{write.serialConnection(con, "Hello World!")}
 #' @export
 write.serialConnection <- function(con, dat) {
-  # TODO: coerce dat to character with toString() (with warning?)
-  stopifnot(is(con, 'serialConnection'),
-            is(dat, 'character'))
+  
+  stopifnot(is(con, 'serialConnection'))
+  if (!is(dat, 'character')) {
+    warning('dat is not a character, coercing to character with toString().')
+    dat <- toString(dat)
+  }
+  
   nl <- ifelse(con$newline, "", "-nonewline ")
   .Tcl(paste("puts ", nl, "$sdev_", con$port, " \"", dat, "\"", sep = ""))
   
@@ -52,8 +59,29 @@ read.serialConnection <- function(con) {
 
 #' Looks for active serial ports.
 #' 
+#' This function checks for active serial ports on the host device.
+#' 
+#' @usage check.devices()
+#' 
+#' @return A character vector with all active devices, or `character(0)` if no device is found.
 check.devices <- function() {
-  os_path <- switch(.Platform$OS.type, windows = "//./", unix = "/dev/")
-  os_prefix <- switch(.Platform$OS.type, windows = "COM", unix = "tty")
-  
+  os <- .Platform$OS.type
+  if (identical(os, "unix")) {
+    temp_dir <- "/dev/serial/by-id"
+    if (file_test("-d", temp_dir)) {
+      # check symbolic links
+      cmmd <- 'ls -l /dev/serial/by-id | grep "\\->"'
+      dev_info <- system(cmmd, intern = TRUE)
+      regmatches(dev_info, regexpr('(?:tty).*', text = dev_info))
+    } else {
+      character(0)
+    }
+    
+  } else if (identical(os, "windows")) {
+    cmmd <- 'REG QUERY HKEY_LOCAL_MACHINE\\HARDWARE\\DEVICEMAP\\SERIALCOMM'
+    dev_info <- system(cmmd, intern = TRUE)
+    regmatches(dev_info, regexpr('(?:COM)[0-9]{1,3}'))
+  } else {
+    stop("Unidentified platform.")
+  }
 }
