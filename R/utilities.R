4d09995629d82ea2239eb211ffd88b731c448f93
Alex Forrence <aforren1@jhu.edu>
2016-03-16 12:04:24 -0400

    Try to get rid of tcltk-related warning, move check_devices to utilities

--- R/utilities.R
+++ R/utilities.R
@@ -1,5 +1,49 @@
 # Utilities
 
+#' Looks for serial ports in use.
+#' 
+#' This function checks for serial ports in use on the host device.
+#' 
+#' @usage check_devices()
+#' 
+#' @return A character vector with all active devices, or `character(0)` if no device is found.
+#' @examples
+#' \dontrun{
+#' available_devices <- check_devices()
+#' }
+#' @importFrom utils file_test
+#' @export
+check_devices <- function() {
+  os <- ifelse(identical(.Platform$OS.type, 'windows'), 'windows', 
+               ifelse(grep('linux', R.version$platform), 'linux', 'osx'))
+  
+  if (identical(os, "linux")) {
+    if (file_test("-d", "/dev/serial/by-id")) { # only exists when device connected
+      # check symbolic links
+      cmmd <- 'ls -l /dev/serial/by-id | grep "\\->"'
+      dev_info <- system(cmmd, intern = TRUE)
+      regmatches(dev_info, regexpr('(?:tty).*', text = dev_info))
+    } else {
+      character(0)
+    }
+  } 
+  
+  else if (identical(os, "windows")) {
+    cmmd <- 'REG QUERY HKEY_LOCAL_MACHINE\\HARDWARE\\DEVICEMAP\\SERIALCOMM'
+    dev_info <- system(cmmd, intern = TRUE)
+    regmatches(dev_info, regexpr('(?:COM)[0-9]{1,3}', text = dev_info))
+  } 
+  
+  else if (identical(os, "osx")) {
+    dev_info <- system('ls /dev/tty.*', intern = TRUE)
+    regmatches(dev_info, regexpr("(?:tty).*", text = dev_info))
+  } 
+  
+  else {
+    stop("Unidentified platform.")
+  }
+}
+
 #' Writes data to the serial interface. 
 #' 
 #' @param con Object of class \code{serialConnection}
