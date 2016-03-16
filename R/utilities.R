ce10e39ca97aaba5ecfc32576679a9bc1da8c8f3
Alex Forrence <aforren1@jhu.edu>
2016-03-16 09:09:47 -0400

    Adding back deprecated functions

--- R/utilities.R
+++ R/utilities.R
@@ -24,6 +24,19 @@
 #' @export
 write_data <- function(con, dat, ...) UseMethod("write_data")
 
+#' Writes data to the serial interface. 
+#'
+#' Deprecated, please use \code{\link{write_data}}.
+#' @param con Object of class \code{serialConnection}
+#' @param dat Data string to write on the serial interface. This can either
+#'                be a character string or any R object that can be coerced
+#'                to a character string by the \code{\link{toString}} function,
+#'                though the latter occurs with a warning. 
+#'                See the example section in \code{\link{serial}}.
+#' @param ... Currently ignored.
+#'@export
+write.serialConnection <- function(con, dat, ...) UseMethod("write_data")
+
 #' Reads from the serial interface.
 #' 
 #' This function reads from the serial interface as long as the buffer is not
@@ -48,3 +61,11 @@ write_data <- function(con, dat, ...) UseMethod("write_data")
 #' @method read_data serialConnection
 #' @export
 read_data <- function(con, ...) UseMethod("read_data")
+
+#' Writes data to the serial interface. 
+#'
+#' Deprecated, please use \code{\link{read_data}}.
+#' @param con Object of class \code{serialConnection}
+#' @param ... Currently ignored.
+#' @export
+read.serialConnection <- function(con, ...) UseMethod("read_data")
