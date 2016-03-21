f2886577c4d3f075291fb6834a3cb9a466aa1174
Alex Forrence <aforren1@jhu.edu>
2016-03-21 11:33:40 -0400

    Don't believe everything you read on the internet

--- R/utilities.R
+++ R/utilities.R
@@ -20,7 +20,7 @@ find_devices <- function() {
       # check symbolic links
       cmmd <- 'ls -l /dev/serial/by-id | grep "\\->"'
       dev_info <- system(cmmd, intern = TRUE)
-      regmatches(dev_info, regexpr('(?:tty).*|(?:cu).*', text = dev_info))
+      regmatches(dev_info, regexpr('(?:tty).*', text = dev_info))
     } else {
       character(0)
     }
@@ -34,7 +34,7 @@ find_devices <- function() {
   
   else if (identical(os, "osx")) {
     dev_info <- system('ls /dev/tty.*', intern = TRUE)
-    regmatches(dev_info, regexpr("(?:tty).*|(?:cu).*", text = dev_info))
+    gsub("/dev/", "", dev_info)
   } 
   
   else {
