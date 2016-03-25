c101d835d465504f13a41add0b253c20758595e5
Alex Forrence <aforren1@jhu.edu>
2016-03-25 14:59:42 -0400

    Try *not* catching bad osx tcltk, I think requiring tcltk covers it?

--- R/hooks.R
+++ R/hooks.R
@@ -1,10 +1,10 @@
-# Try to catch incomplete tcltk on osx
-.onAttach <- function(...) {
-  if (grepl("darwin", R.version$os) & 
-      length(list.files("/opt/X11/bin", pattern = "Xquartz")) == 0) {
-    packageStartupMessage(paste("X11 is required. Please visit",
-                                "http://xquartz.macosforge.org",
-                                "to download and install Xquartz."))
-    stop()
-  } 
-}
+# # Try to catch incomplete tcltk on osx
+# .onAttach <- function(...) {
+#   if (grepl("darwin", R.version$os) & 
+#       length(list.files("/opt/X11/bin", pattern = "Xquartz")) == 0) {
+#     packageStartupMessage(paste("X11 is required. Please visit",
+#                                 "http://xquartz.macosforge.org",
+#                                 "to download and install Xquartz."))
+#     stop()
+#   } 
+# }
